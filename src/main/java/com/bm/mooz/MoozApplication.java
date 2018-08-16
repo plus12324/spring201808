3a5ad36bf35fda79741933bf4ac2840cd101e446
plus12324 <sung-bm@hanmail.net>
2018-08-17 01:23:52 +0900

    init1

---
+++ src/main/java/com/bm/mooz/MoozApplication.java
@@ -0,0 +1,12 @@
+package com.bm.mooz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoozApplication.class, args);
+	}
+}
